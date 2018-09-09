847513b780907ae9ffa8e8a7e4ecf5d2881c8f92
Rajvir Rai <rsr1992@gmail.com>
2018-09-09 13:42:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if fizzbuzz(3)
+  if int % 3 == 0 
+  fizz_3
    return "Fizz"
   end
 end
